99940c9b7ab21718a27ccefddb1b19c489ff3a57
Jan Rabe <jan.rabe@exozet.com>
2019-06-11 21:30:22 +0200

    fixed splash

--- app/src/main/java/net/kibotu/schlachtensee/ui/splash/SplashFragment.kt
+++ app/src/main/java/net/kibotu/schlachtensee/ui/splash/SplashFragment.kt
@@ -1,5 +1,7 @@
 package net.kibotu.schlachtensee.ui.splash
 
+import androidx.navigation.fragment.findNavController
+import com.exozet.android.core.extensions.resLong
 import net.kibotu.schlachtensee.R
 import net.kibotu.schlachtensee.ui.base.BaseFragment
 
@@ -7,7 +9,23 @@ import net.kibotu.schlachtensee.ui.base.BaseFragment
  * Created by <a href="https://about.me/janrabe">Jan Rabe</a>.
  */
 
-class SplashFragment  : BaseFragment() {
+class SplashFragment : BaseFragment() {
 
     override val layout = R.layout.fragmnet_splash
+
+    val navigate = Runnable {
+        navigateToNextScreen()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        view?.postDelayed(navigate, R.integer.splash_duration.resLong)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        view?.removeCallbacks(navigate)
+    }
+
+    private fun navigateToNextScreen() = findNavController().navigate(R.id.action_splash_to_current_temperature)
 }
--- app/src/main/java/net/kibotu/schlachtensee/ui/temperature/CurrentTemperatureFragment.kt
+++ app/src/main/java/net/kibotu/schlachtensee/ui/temperature/CurrentTemperatureFragment.kt
@@ -1,13 +1,29 @@
 package net.kibotu.schlachtensee.ui.temperature
 
+import com.exozet.android.core.extensions.onClick
+import kotlinx.android.synthetic.main.fragment_current_temperature.*
 import net.kibotu.schlachtensee.R
 import net.kibotu.schlachtensee.ui.base.BaseFragment
+import kotlin.random.Random
+
 
 /**
  * Created by <a href="https://about.me/janrabe">Jan Rabe</a>.
  */
 
-class CurrentTemperatureFragment :BaseFragment(){
+class CurrentTemperatureFragment : BaseFragment() {
 
     override val layout = R.layout.fragment_current_temperature
+
+    override fun subscribeUi() {
+        super.subscribeUi()
+
+        thermometer.onClick {
+            thermometer.setValueAndStartAnim(Random.nextFloat() * 7 + 35)
+        }
+    }
+
+    override fun unsubscribeUi() {
+        super.unsubscribeUi()
+    }
 }
